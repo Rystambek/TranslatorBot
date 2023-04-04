50d6d7d94dff211b31e5ff2f279432b0fdb283ac
Rystambek <rustambekjumaqulov@gmail.com>
2023-04-04 20:21:06 +0500

    tarjimon handler

--- bot.py
+++ bot.py
@@ -1,1 +1,15 @@
-from telegram import KeyboardButton
+from telegram.ext import Updater,Filters,CommandHandler,MessageHandler
+from telegram import Update
+from handler import start,tarjimon
+
+TOKEN = '6004154698:AAEo2pZT8WqoCqRGAXZchoYMtdgozcq3Kbc'
+
+updater = Updater(token=TOKEN)
+
+dp = updater.dispatcher
+
+dp.add_handler(CommandHandler('start',start))
+dp.add_handler(MessageHandler(Filters.update,tarjimon))
+
+updater.start_polling()
+updater.idle()
