e60df7ecebd95c5c9d49cd17f85725dbd6cd2f2a
Rystambek <rustambekjumaqulov@gmail.com>
2023-04-05 11:54:18 +0500

    translator bot

--- handler.py
+++ handler.py
@@ -1,8 +1,6 @@
 from telegram.ext import Updater,Filters,CallbackContext
 from telegram import Update
 from googletrans import Translator
-from lookup import query
-
 tranlator = Translator()
 
 def start(update:Update,context:CallbackContext):
@@ -13,21 +11,8 @@ def start(update:Update,context:CallbackContext):
 def tarjimon(update:Update,context:CallbackContext):
     m_text = update.message.text
     lang = tranlator.detect(m_text).lang
-    
     print(lang)
-    if len(m_text.split()) >= 2:
-        if lang == 'en':
-            update.message.reply_text(tranlator.translate(m_text,dest='uz').text)
-        else:
-            update.message.reply_text(tranlator.translate(m_text,dest='en').text)
+    if lang == 'en':
+        update.message.reply_text(tranlator.translate(m_text,dest='uz').text)
     else:
-        if lang == 'en':
-            word_id = m_text
-            word = query(word_id)
-        else:
-            word_id = tranlator.translate(m_text,dest='en').text
-            word = query(word_id)
-        if word:
-            update.message.reply_text(f"Word: {word_id} \nDefinitions: \n{word['definitions']}")
-        else:
-            update.message.reply_text('Bunday so\'z topilmadi')
+        update.message.reply_text(tranlator.translate(m_text,dest='en').text)
